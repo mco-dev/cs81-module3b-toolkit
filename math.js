58f2063160423c1d17a114db468fdd17b7c274d7
Marc Overton <mco.dev@gmail.com>
2026-01-17 12:56:30 -0800

    Added isEven() function.

---
+++ math.js
@@ -0,0 +1,7 @@
+function double(n) {
+    return n * 2; // Return the number and multiplied by 2
+}
+
+function isEven(n) {
+    return n % 2 == 0; // Return true if the number is even, else false.
+}
